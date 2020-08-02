947e0f53c5496294b8a5c26836864e89deb2efdc
anmadri3 <anmadri3@asu.edu>
2020-08-02 11:28:56 -0700

    added changes to database file

--- ProjectTemplate/javascript/database.js
+++ ProjectTemplate/javascript/database.js
@@ -1,7 +1,6 @@
-function insertIntoDb(buttonType, text, anon, email){
-    var mysql = require('mysql');
+var mysql = require('mysql');
 
-    var con = mysql.createConnection({
+var con = mysql.createConnection({
     host: "107.180.1.16",
     user: "summer2020group2",
     password: "!!Group2",
@@ -11,14 +10,11 @@ function insertIntoDb(buttonType, text, anon, email){
     con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    //Insert a record in the "customers" table:
-    var sql = "INSERT INTO surveyResponses (buttonClicked, feedback, anonymous, email) VALUES (buttonType, text, anon, email)";
+    var sql = "INSERT INTO surveyResponses (buttonClicked, feedback, anonymous, email) VALUES ('mad', 'some text', 1, 'email@gmail.com')";
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("1 record inserted");
     });
-    });
+});
 
-
-}
 
